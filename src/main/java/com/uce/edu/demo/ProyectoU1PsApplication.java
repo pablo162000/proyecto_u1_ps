784a9ae8fb0b1032b90c23997c9ad9fc2a8dcd70
Pablo Suntaxi <pbsun.ale@gmail.com>
2022-05-27 20:05:12 -0500

    Mi primera subida desde consola

---
+++ src/main/java/com/uce/edu/demo/ProyectoU1PsApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU1PsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU1PsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("MI primer proyecto Spring framework");
+	}
+
+}
---
+++ src/main/java/com/uce/edu/demo/consultorio/Doctor.java
@@ -0,0 +1,5 @@
+package com.uce.edu.demo.consultorio;
+
+public class Doctor {
+
+}
